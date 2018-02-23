7bb8208443399f8e864ded0b1ecec78e6e4ef4d5
Charles Fahselt <Charles.Fahselt@gmail.com>
2018-02-22 23:40:38 -0600

    and more bullets!

--- menu_state.js
+++ menu_state.js
@@ -5,8 +5,13 @@ var menu_state = {
         var space_to_begin_text = Utils.create_centered_text('Press Space to begin', 850, 35);
 
         var now_with_text = Utils.create_text('Now with a PERMANENT\nleaderboard!', 1100, 550, 20, "#FFD700");
+        var now_with_text2 = Utils.create_text('and way more bullets', 1100, 600, 10, "#FFD700");
         now_with_text.angle = -2;
         game.add.tween(now_with_text).to({ angle: 2 }, 5000, function(k) {
+           return Math.sin(Math.PI * 2 * k);
+         }, true, 0, -1);
+
+        game.add.tween(now_with_text2).to({ angle: 2 }, 5000, function(k) {
            return Math.sin(Math.PI * 2 * k);
          }, true, 0, -1);
 
