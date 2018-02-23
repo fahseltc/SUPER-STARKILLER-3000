2c9f540e2c11a06693907e70f2c5838781963bb7
Charles Fahselt <Charles.Fahselt@gmail.com>
2018-02-22 21:25:23 -0600

    clean up indents

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <!doctype html>
 <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <link rel="stylesheet" type = "text/css" href = "font_loader.css">
-        <script src="phaser.min.js"></script>
-        <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-        <title>Mega Mecha Marxist II</title>
-    </head>
-    <body>
-        <script type="text/javascript">
-            // the grossest, 'never used php before' hack to get heroku environment variables in a client-side js game.
-            var div = document.createElement('div');
-            div.id = 'environment';
-            div.hidden = true;
-            div.appendChild(document.createTextNode("<?php echo getenv('environment')?>"));
-            document.body.appendChild(div);
-        </script>
+  <head>
+    <link rel="stylesheet" type = "text/css" href = "font_loader.css">
+    <script src="phaser.min.js"></script>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <title>Mega Mecha Marxist II</title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      // the grossest, 'never used php before' hack to get heroku environment variables in a client-side js game.
+      var div = document.createElement('div');
+      div.id = 'environment';
+      div.hidden = true;
+      div.appendChild(document.createTextNode("<?php echo getenv('environment')?>"));
+      document.body.appendChild(div);
+    </script>
 
-        <script type="text/javascript" src="controls.js"></script>
-        <script type="text/javascript" src="utils.js"></script>
-        <script type="text/javascript" src="game_timer.js"></script>
-        <script type="text/javascript" src="score.js"></script>
-        <script type="text/javascript" src="enemy_manager.js"></script>
-        <script type="text/javascript" src="mecha.js"></script>
-        <script type="text/javascript" src="circle_weapon.js"></script>
-        <script type="text/javascript" src="menu_state.js"></script>
-        <script type="text/javascript" src="play_state.js"></script>
-        <script type="text/javascript" src="load_state.js"></script>
-        <script type="text/javascript" src="boot_state.js"></script>
-        <script type="text/javascript" src="post_game_state.js"></script>
-        <script type="text/javascript" src="leaderboard_state.js"></script>
-        <script type="text/javascript" src="main.js"></script>
-        <div id="game" style="margin:0 auto;"></div>
-    </body>
+    <script type="text/javascript" src="controls.js"></script>
+    <script type="text/javascript" src="utils.js"></script>
+    <script type="text/javascript" src="game_timer.js"></script>
+    <script type="text/javascript" src="score.js"></script>
+    <script type="text/javascript" src="enemy_manager.js"></script>
+    <script type="text/javascript" src="mecha.js"></script>
+    <script type="text/javascript" src="circle_weapon.js"></script>
+    <script type="text/javascript" src="menu_state.js"></script>
+    <script type="text/javascript" src="play_state.js"></script>
+    <script type="text/javascript" src="load_state.js"></script>
+    <script type="text/javascript" src="boot_state.js"></script>
+    <script type="text/javascript" src="post_game_state.js"></script>
+    <script type="text/javascript" src="leaderboard_state.js"></script>
+    <script type="text/javascript" src="main.js"></script>
+    <div id="game" style="margin:0 auto;"></div>
+  </body>
 </html>
