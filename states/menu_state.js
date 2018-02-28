f6ebef06f6a7d8ba86a4d7edd113276d49d0b4f0
Charles Fahselt <Charles.Fahselt@gmail.com>
2018-02-27 21:07:08 -0600

    menu text

--- states/menu_state.js
+++ states/menu_state.js
@@ -24,8 +24,8 @@ var menu_state = {
 
     var space_to_begin_text = Utils.create_centered_text('Press Space to begin', 850, 35);
 
-    var now_with_text = Utils.create_text('Cool enemy swooshes\nand spacebar!', 1100, 550, 20, "#FFD700");
-    var now_with_text2 = Utils.create_text('and less difficulty?\nbecause its level 1', 1100, 600, 10, "#FFD700");
+    var now_with_text = Utils.create_text('Now with\nTimed Levels!!', 1100, 550, 20, "#FFD700");
+    var now_with_text2 = Utils.create_text('and if ur good enough it crashes!', 1100, 600, 10, "#FFD700");
     now_with_text.angle = -2;
     game.add.tween(now_with_text).to({ angle: 2 }, 5000, function(k) {
        return Math.sin(Math.PI * 2 * k);
