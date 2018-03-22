d576db9524045de22b15263d689715f7a312cc20
Charles Fahselt <Charles.Fahselt@gmail.com>
2018-03-21 22:22:45 -0500

    typo

--- ui/shield_weapon_display.js
+++ ui/shield_weapon_display.js
@@ -2,7 +2,7 @@ class ShieldWeaponDisplay {
   constructor(player) {
     this.player = player;
 
-    this.green_bar = game.add.sprite(1216, 880, 'green_bar');
+    this.green_bar = game.add.sprite(1216, 880, "green_bar");
     this.green_bar.anchor.set(0.5, 1);
     this.original_height = this.green_bar.height;
     this.green_bar.height = 0;
@@ -11,14 +11,31 @@ class ShieldWeaponDisplay {
   }
 
   update() {
-    if(this.player.circle_weapon.active && !this.rising) {
+    if (this.player.circle_weapon.active && !this.rising) {
       this.rising = true;
-      var tween = game.add.tween(this.green_bar).to( {height: this.original_height}, 50, Phaser.Easing.Linear.None, true);
-      tween.onComplete.addOnce(function() { this.rising = false; }, this);
-    } else if(!this.player.circle_weapon.active && this.green_bar.height == this.original_height && !this.falling) {
+      var tween = game.add
+        .tween(this.green_bar)
+        .to(
+          { height: this.original_height },
+          50,
+          Phaser.Easing.Linear.None,
+          true
+        );
+      tween.onComplete.addOnce(function() {
+        this.rising = false;
+      }, this);
+    } else if (
+      !this.player.circle_weapon.active &&
+      this.green_bar.height == this.original_height &&
+      !this.falling
+    ) {
       this.falling = true;
-      var tween = game.add.tween(this.green_bar).to( {height: 0}, 100, Phaser.Easing.Linear.None, true);
-      tween.onComplete.addOnce(function() { this.falling = false; }, this);
+      var tween = game.add
+        .tween(this.green_bar)
+        .to({ height: 0 }, 100, Phaser.Easing.Linear.None, true);
+      tween.onComplete.addOnce(function() {
+        this.falling = false;
+      }, this);
     }
   }
 }
